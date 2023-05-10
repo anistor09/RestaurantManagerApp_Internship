1a7c3e122d4b86bf35b08d571a73474fd8c80cf7
Vian Robotin <V.Robotin@student.tudelft.nl>
2023-05-10 18:59:28 +0200

    Added a default layout

---
+++ layouts/default.vue
@@ -0,0 +1,29 @@
+<template>
+    <el-container>
+        <el-aside>
+            <el-menu default-active="2" background-color="pink" style="height: 100vh;">
+                <el-menu-item index="1">
+                    <NuxtLink to="/">Home</NuxtLink>
+                </el-menu-item>
+                <el-menu-item index="2">
+                    <NuxtLink to="/helloWorld">Hello</NuxtLink>
+                </el-menu-item>
+                <el-menu-item index="3">
+                    <NuxtLink to="/">Page 3</NuxtLink>
+                </el-menu-item>
+                <el-menu-item index="4">
+                    <NuxtLink to="/">Page 4</NuxtLink>
+                </el-menu-item>
+            </el-menu>
+        </el-aside>
+        <slot></slot>
+    </el-container>
+</template>
+  
+  
+<style>
+.router-link-exact-active {
+    font-weight: bold;
+}
+</style>
+  
