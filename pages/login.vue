357b41e0eaf8452268ea360e72f7f71ca87a7ef2
Elena Dumitrescu <e.i.dumitrescu-1@student.tudelft.nl>
2023-05-13 16:30:45 +0200

    Add comments, clean up code

--- pages/login.vue
+++ pages/login.vue
@@ -1,17 +1,24 @@
 <script lang="ts" setup></script>
 
+<!-- The login page. Can be accessed at: http://localhost:3000/login -->
 <template>
 	<div id="wrapper">
+        <!-- Styling images for the background. For now, they are stored on client, but they should be moved on the server. -->
 		<img id="imgDown" src="../uploads/loginImage1.png" />
 		<img id="imgUp" src="../uploads/loginImage2.png" />
+
+        <!-- The logo and subtitle are wrapped together for resizing purposes -->
 		<div id="logoWrapper">
 			<img id="logo" src="../uploads/ewai-logo.png" />
+            <!-- The slogan has multiple colors, thus the need of "capitalLetters" class -->
 			<div id="subtitle">
-				The <span class="capital-letters">E</span>lectronic
-				<span class="capital-letters">WAI</span>ter. Without the
-				<span class="capital-letters">WAI</span>t.
+				The <span class="capitalLetters">E</span>lectronic
+				<span class="capitalLetters">WAI</span>ter. Without the
+				<span class="capitalLetters">WAI</span>t.
 			</div>
 		</div>
+
+        <!-- Similarly, the button and the welcome text are wrapped and, thus, resized, together -->
 		<div id="buttonWrapper">
 			<div id="welcome">WELCOME BACK!</div>
 			<el-button id="button" color="#ED5087" plain round> Login </el-button>
@@ -23,6 +30,7 @@
 @import url('https://fonts.googleapis.com/css?family=Tourney');
 @import url('https://fonts.googleapis.com/css?family=Cairo');
 
+/* The images are sent in the back using z-index since they overlapped with the button */
 #imgUp {
 	position: fixed;
 	left: 60%;
@@ -60,7 +68,7 @@
 	font-family: 'Tourney';
 }
 
-#subtitle .capital-letters {
+#subtitle .capitalLetters {
 	color: #ed5087;
 }
 
@@ -74,6 +82,7 @@
 	align-items: center;
 }
 
+/* The styling of the Element Plus is overwritten to fit our Figma design */
 #button {
 	font-family: 'Cairo' !important;
 	font-size: 2vw;
@@ -91,6 +100,7 @@
 	padding-top: 17%;
 }
 
+/* Mobile version does not contain the design images, just a basic white background */
 @media only screen and (max-width: 650px) {
 	#imgDown,
 	#imgUp {
