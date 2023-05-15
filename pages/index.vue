8fdbd5a02b43231a1f3bd76efb59f718d3de9613
Vian Robotin <V.Robotin@student.tudelft.nl>
2023-05-15 12:13:02 +0200

    Fix index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,14 @@
 <script lang="ts" setup>
-import ItemComponent from '~/components/item-component.vue';
-import CategoryComponent from '~/components/category-component.vue';
 </script>
 
 <template>
-	<el-row>
-    <el-col :span="5"><ItemComponent /></el-col>
-    <el-col :span="5"><CategoryComponent /></el-col>
-  </el-row>
+  <div>
+    <RestaurantComponent/>
+  <div><el-row>
+      <el-col :span="5"><ItemComponent /></el-col>
+      <el-col :span="5"><CategoryComponent /></el-col>
+    </el-row></div>
+  </div>
 </template>
 
 <style scoped></style>
