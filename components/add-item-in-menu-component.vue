53ede82e69271020fcae9f8ae57c502391faceb8
Robert Vadastreanu <R.V.Vadastreanu-1@student.tudelft.nl>
2023-05-17 13:05:42 +0200

    Fix pipeline

--- components/add-item-in-menu-component.vue
+++ components/add-item-in-menu-component.vue
@@ -1,86 +1,101 @@
 <script lang="ts" setup>
-
-import { filter } from 'lodash';
 import restaurants from '../mockData/restaurants.json';
 
-
-const restaurant = ref(restaurants.filter(x => x.id == 1)[0]);
+const restaurant = ref(restaurants.filter((x) => x.id === 1)[0]);
 
 const categories = ref(restaurant.value.categorySet);
-const categoryName = ref("") //this field is the name of the selected category from the el-select
-const selectedCategory = ref(categories.value[0]) // we assume that there is always an category in the database (because we need to use category.Id)
+const categoryName = ref(''); 
+const selectedCategory = ref(categories.value[0]); 
 
-const subcategoryName = ref("");
+const subcategoryName = ref('');
 const filteredSubcategories = ref(restaurant.value.subCategorySet);
 const selectedSubcategory = ref(filteredSubcategories.value[0]);
 
 const restaurantItems = ref(restaurant.value.itemSet);
 const filteredItems = ref(restaurant.value.itemSet);
-const itemName = ref("");
+const itemName = ref('');
 const selectedItem = ref(restaurantItems.value[0]);
 
-const enableSubcategory = ref(false)
-const enableItems = ref(false)
-
+const enableSubcategory = ref(false);
+const enableItems = ref(false);
 
 const www = () => {
-  console.log(categories.value[0])
-}
+	console.log(categories.value[0]);
+};
 
 const changeCategory = () => {
-  enableSubcategory.value = true;
-  subcategoryName.value = "";
-  enableItems.value = false;
-  itemName.value = "";
-  selectedCategory.value = categories.value.filter(x => x.name === categoryName.value)[0]
-  filteredSubcategories.value = selectedCategory.value.subCategorySet;
-}
+	enableSubcategory.value = true;
+	subcategoryName.value = '';
+	enableItems.value = false;
+	itemName.value = '';
+	selectedCategory.value = categories.value.filter((x) => x.name === categoryName.value)[0];
+	filteredSubcategories.value = selectedCategory.value.subCategorySet;
+};
 
 const changeSubCategory = () => {
-  enableItems.value = true;
-  itemName.value = "";
-  selectedSubcategory.value = filteredSubcategories.value.filter(x => x.name == subcategoryName.value)[0];
-  filteredItems.value = restaurantItems.value.filter(x => x.category.id == selectedCategory.value.id
-                                                            && x.subCategory?.id == selectedSubcategory.value.id)
-}
-
+	enableItems.value = true;
+	itemName.value = '';
+	selectedSubcategory.value = filteredSubcategories.value.filter(
+		(x) => x.name === subcategoryName.value,
+	)[0];
+	filteredItems.value = restaurantItems.value.filter(
+		(x) =>
+			x.category.id === selectedCategory.value.id &&
+			x.subCategory?.id === selectedSubcategory.value.id,
+	);
+};
 
 const changeItem = () => {
-  selectedItem.value = filteredItems.value.filter(x => x.name == itemName.value)[0];
-}
+	selectedItem.value = filteredItems.value.filter((x) => x.name === itemName.value)[0];
+};
 </script>
 
 <template>
- 
-    <button @click="www"> </button>
-    
+  <div class="All">
+    <button @click="www"></button>
+
     <div class="div">
       <h1>Category:</h1>
 
       <el-select v-model="categoryName" placeholder="Select" size="large" @change="changeCategory">
-          <el-option v-for="category in categories" :key="category.id" :label="category.name"
-              :value="category.name" />
+        <el-option
+          v-for="category in categories"
+          :key="category.id"
+          :label="category.name"
+          :value="category.name"
+        />
       </el-select>
-
     </div>
 
     <div v-if="enableSubcategory" class="div">
       <h1>SubCategory:</h1>
 
-      <el-select v-model="subcategoryName" placeholder="Select" size="large" @change="changeSubCategory">
-          <el-option v-for="subcategory in filteredSubcategories" :key="subcategory.id" :label="subcategory.name"
-              :value="subcategory.name" />
+      <el-select
+        v-model="subcategoryName"
+        placeholder="Select"
+        size="large"
+        @change="changeSubCategory"
+      >
+        <el-option
+          v-for="subcategory in filteredSubcategories"
+          :key="subcategory.id"
+          :label="subcategory.name"
+          :value="subcategory.name"
+        />
       </el-select>
-
     </div>
-   
+
     <div v-if="enableItems" class="div">
       <h1>Item:</h1>
 
       <el-select v-model="itemName" placeholder="Select" size="large" @change="changeItem">
-          <el-option v-for="item in filteredItems" :key="item.id" :label="item.name"
-              :value="item.name" />
+        <el-option
+          v-for="item in filteredItems"
+          :key="item.id"
+          :label="item.name"
+          :value="item.name"
+        />
       </el-select>
     </div>
-
+</div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,4 @@
 <script lang="ts" setup>
-import ItemComponent from '~/components/item-component.vue';
-import CategoryComponent from '~/components/category-component.vue';
 import AddItemInMenuComponent from '~/components/add-item-in-menu-component.vue';
 </script>
 
