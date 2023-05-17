6168e445652ea2f7a3c864029368c44f08be3349
Robert Vadastreanu <R.V.Vadastreanu-1@student.tudelft.nl>
2023-05-17 15:21:55 +0200

    Added style

--- components/add-item-in-menu-component.vue
+++ components/add-item-in-menu-component.vue
@@ -4,8 +4,8 @@ import restaurants from '../mockData/restaurants.json';
 const restaurant = ref(restaurants.filter((x) => x.id === 1)[0]);
 
 const categories = ref(restaurant.value.categorySet);
-const categoryName = ref(''); 
-const selectedCategory = ref(categories.value[0]); 
+const categoryName = ref('');
+const selectedCategory = ref(categories.value[0]);
 
 const subcategoryName = ref('');
 const filteredSubcategories = ref(restaurant.value.subCategorySet);
@@ -51,52 +51,134 @@ const changeItem = () => {
 </script>
 
 <template>
-  <div class="All">
-    <button @click="www"></button>
+	<div id="popup">
+		<div id="all">
+			<div class="div">
+				<h1 class="title">Category:</h1>
 
-    <div class="div">
-      <h1>Category:</h1>
+				<el-select
+					class="specialSelect"
+					v-model="categoryName"
+					placeholder="Select category"
+					size="large"
+					@change="changeCategory"
+				>
+					<el-option
+						v-for="category in categories"
+						:key="category.id"
+						:label="category.name"
+						:value="category.name"
+					/>
+				</el-select>
+			</div>
 
-      <el-select v-model="categoryName" placeholder="Select" size="large" @change="changeCategory">
-        <el-option
-          v-for="category in categories"
-          :key="category.id"
-          :label="category.name"
-          :value="category.name"
-        />
-      </el-select>
-    </div>
+			<div class="div">
+				<h1 class="title">SubCategory:</h1>
 
-    <div class="div">
-      <h1>SubCategory:</h1>
+				<el-select
+					class="specialSelect"
+					v-model="subcategoryName"
+					:disabled="!enableSubcategory"
+					placeholder="Select subcategory"
+					size="large"
+					@change="changeSubCategory"
+				>
+					<el-option
+						v-for="subcategory in filteredSubcategories"
+						:key="subcategory.id"
+						:label="subcategory.name"
+						:value="subcategory.name"
+					/>
+				</el-select>
+			</div>
 
-      <el-select
-        v-model="subcategoryName"
-        :disabled="!enableSubcategory"
-        placeholder="Select"
-        size="large"
-        @change="changeSubCategory"
-      >
-        <el-option
-          v-for="subcategory in filteredSubcategories"
-          :key="subcategory.id"
-          :label="subcategory.name"
-          :value="subcategory.name"
-        />
-      </el-select>
-    </div>
+			<div class="div">
+				<h1 class="title">Item:</h1>
 
-    <div class="div">
-      <h1>Item:</h1>
-
-      <el-select v-model="itemName" placeholder="Select" size="large" :disabled="!enableItems" @change="changeItem">
-        <el-option
-          v-for="item in filteredItems"
-          :key="item.id"
-          :label="item.name"
-          :value="item.name"
-        />
-      </el-select>
-    </div>
-</div>
+				<el-select
+					class="specialSelect"
+					v-model="itemName"
+					placeholder="Select item"
+					size="large"
+					:disabled="!enableItems"
+					@change="changeItem"
+				>
+					<el-option
+						v-for="item in filteredItems"
+						:key="item.id"
+						:label="item.name"
+						:value="item.name"
+					/>
+				</el-select>
+			</div>
+			<div id="buttonContainer">
+				<el-button color="#ED5087" plain round> Add</el-button>
+			</div>
+		</div>
+	</div>
 </template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css?family=Cairo');
+
+#popup {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	width: 100%;
+	height: 100%;
+	font-family: 'Cairo';
+}
+#all {
+	width: 35%;
+	height: 60%;
+	display: flex;
+	align-items: center;
+	flex-direction: column;
+	justify-content: space-around;
+  border: 10px solid #ED5087;
+  border-radius: 100px;
+  padding: 20px;
+}
+#buttonContainer {
+	display: flex;
+	justify-content: center;
+	padding: 5%;
+	align-items: center;
+}
+
+.specialSelect >>> .el-input__wrapper{
+  border-radius: 25px;
+  background-color: #D9D9D9;
+  padding-left: 4%;
+  padding-right: 4%;
+  width: 100%; 
+  height: 100%;
+}
+.specialSelect >>> .el-input__inner {
+	color: black;
+	font-size: 2vh;
+}
+
+.el-select {
+	width: 100%;
+	height: 30%;
+}
+.specialSelect >>> .select-trigger {
+	width: 100%;
+	height: 100%;
+}
+.div {
+  width: 60%;
+}
+.title {
+  color: #ED5087;
+}
+.specialSelect >>> .el-input {
+	width: 100%;
+	height: 100%;
+}
+.el-button+.el-button{
+  margin-left: 0;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -34,7 +34,7 @@
 </template>
   
   
-<style>
+<style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Solway&display=swap');
 span{
     padding-left: 20%;
