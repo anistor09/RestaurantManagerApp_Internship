8e9fe311e01202ff93b439747d01773ef8994848
Alexandru Nistor <a.nistor@student.tudelft.nl>
2023-05-25 14:12:22 +0200

    Add Edit/Add Category Page

---
+++ pages/editCategoryView/index.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup></script>
+
+<template>
+	<div class="container">
+		<div class="right">
+			<editCategory :add-category="true"/>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+.right {
+	width: 100%;
+	height: 100vh;
+}
+</style>
