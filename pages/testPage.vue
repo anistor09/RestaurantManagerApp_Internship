6968eba58ea6aaad598f106326fcae48b9a0e746
Robert Vadastreanu <R.V.Vadastreanu-1@student.tudelft.nl>
2023-06-01 20:49:52 +0200

    Test page

---
+++ pages/testPage.vue
@@ -0,0 +1,11 @@
+<script>
+import { useRestaurantStore } from '../store/restaurant';
+import { Restaurant } from '../interfaces/Restaurant';
+const restaurantStore = useRestaurantStore();
+const restaurant = restaurantStore.restaurantGetter;
+console.log(restaurant.name)
+</script>
+
+<template>
+    <div>{{useRestaurantStore().restaurantGetter.name}}</div>
+</template>
