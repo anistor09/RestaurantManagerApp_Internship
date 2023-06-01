a49d7e6034544e5360df9288acd696db68fc481e
Vian Robotin <V.Robotin@student.tudelft.nl>
2023-06-01 21:55:12 +0200

    Vitest works!

--- test/menus.test.ts
+++ test/menus.test.ts
@@ -1,7 +1,7 @@
 import { vi, it, expect } from 'vitest';
 import { mount } from '@vue/test-utils';
 import restaurants from '../mockData/restaurants.json';
-import testPage from '../pages/menus.vue';
+import MenusPage from '../pages/menus.vue';
 import EditMenuComponent from '../components/edit-menu-component.vue';
 import AddItemInMenuComponent from '../components/add-item-in-menu-component.vue';
 import AddMenuComponent from '../components/add-menu-component.vue';
@@ -18,6 +18,37 @@ vi.mock('../store/restaurant.ts', () => {
 });
 
 it('renders menus page', () => {
-	const wrapper = mount(testPage);
+	const wrapper = mount(MenusPage);
 	expect(wrapper.exists()).toBe(true);
+});
+
+it('opens add menu dialog', async () => {
+	const wrapper = mount(MenusPage);
+	const addMenuButton = wrapper.find('#menus-steps');
+	expect(addMenuButton.exists()).toBe(true);
+	expect(addMenuButton.text()).toBe(
+		'Step 1: Select your menu \nStep 2: Choose the category \nStep 3: Choose the subcategory \nStep 4: Happy editing!',
+	);
+});
+
+it('opens edit menu dialog', async () => {
+	const wrapper = mount(MenusPage);
+	const editMenuButton = wrapper.find('[data-testid="edit-menu-button"]');
+	await editMenuButton.trigger('click');
+	const editMenuComponent = wrapper.findComponent(EditMenuComponent);
+	expect(editMenuComponent.exists()).toBe(true);
+});
+
+it('opens add item dialog', async () => {
+	const wrapper = mount(MenusPage);
+	const addItemButton = wrapper.find('[data-testid="place-item-button"]');
+	await addItemButton.trigger('click');
+	const addItemComponent = wrapper.findComponent(AddItemInMenuComponent);
+	expect(addItemComponent.exists()).toBe(true);
+});
+
+it('renders add menu component', () => {
+	const wrapper = mount(MenusPage);
+	const addMenuComponent = wrapper.findComponent(AddMenuComponent);
+	expect(addMenuComponent.exists()).toBe(true);
 });
