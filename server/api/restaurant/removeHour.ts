a74f494d3868fa1773fc2780b556af2ea981af05
Robert Vădăstreanu <r.v.vadastreanu-1@student.tudelft.nl>
2023-06-05 09:25:42 +0200

    Resolve "Finish functionality for the settings"

--- server/api/restaurant/removeHour.ts
+++ server/api/restaurant/removeHour.ts
@@ -1,8 +1,8 @@
 export default defineEventHandler(async(event) => {
 	const data = await readBody(event);
-	const response = await fetch(`https://dev-api.ewai.fr/restaurant/1`, {
-		method: 'PUT',
-		body: JSON.stringify(data),
+	const response = await fetch(`https://dev-api.ewai.fr/restaurant/{id}/hours/remove?restaurantId=1'`, {
+		method: 'POST',
+        body: JSON.stringify(data),
 		headers: {
 			'Content-Type': 'application/json',
 		},
