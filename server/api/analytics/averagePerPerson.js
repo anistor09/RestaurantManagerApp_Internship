7cc1c3b6ea87ecfdd10f1500f5a9d435e70bb487
Vian Robotin <V.Robotin@student.tudelft.nl>
2023-06-17 17:19:56 +0200

    Replace analytics endpoint

--- server/api/analytics/averagePerPerson.js
+++ server/api/analytics/averagePerPerson.js
@@ -1,5 +1,5 @@
 export default defineEventHandler(async () => {
-	const response = await fetch(`http://localhost:8085/analytics/averagePerson`, {
+	const response = await fetch(`https://dev-api.ewai.fr/analytics/averagePerson`, {
 	});
 	const responseData = await response.json();
 	return responseData;
