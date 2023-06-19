ff766f56104734a69443207799b7a5f34e657205
Vian Robotin <V.Robotin@student.tudelft.nl>
2023-06-19 15:36:15 +0200

    Resolve table bug

--- pages/tables.vue
+++ pages/tables.vue
@@ -8,7 +8,6 @@ import SearchBar from '../components/search-bar.vue';
 import { useLanguageStore } from '../store/language';
 import translations from '../mockData/translations.json';
 
-
 const languageStore = useLanguageStore();
 
 const computedLanguageId = computed(() => languageStore.idGetter);
@@ -32,7 +31,10 @@ const filteredTables = computed(() => {
 	if (!selectedTable.value) {
 		return tables.value;
 	}
-	return tables.value.filter((table) => `Table ${table.number}` === selectedTable.value);
+	return tables.value.filter(
+		(table) =>
+			`${translations[computedLanguageId.value].table} ${table.number}` === selectedTable.value,
+	);
 });
 async function handleDelete() {
 	await useFetch(`/api/table/${tableId.value}`);
@@ -108,16 +110,20 @@ watchEffect(() => {
 			<el-dialog v-model="editPopup" width="20%" style="border-radius: 5%; height: 22%">
 				<div class="edit">
 					<div>
-						<span>{{translations[computedLanguageId].tableNumber}} </span
+						<span>{{ translations[computedLanguageId].tableNumber }} </span
 						><input id="addtableInput3" v-model="tableNumber" class="specialInput" />
 					</div>
 					<div style="padding-top: 2%">
-						<span>{{translations[computedLanguageId].tableCapacity}}</span
+						<span>{{ translations[computedLanguageId].tableCapacity }}</span
 						><input id="addtableInput4" v-model="tableCapacity" class="specialInput" />
 					</div>
 					<div style="padding-top: 5%">
-						<el-button color="#ED5087" plain round @click="editPopup = false">{{translations[computedLanguageId].cancel}}</el-button>
-						<el-button color="#ED5087" plain round @click="handleEdit()">{{translations[computedLanguageId].save}}</el-button>
+						<el-button color="#ED5087" plain round @click="editPopup = false">{{
+							translations[computedLanguageId].cancel
+						}}</el-button>
+						<el-button color="#ED5087" plain round @click="handleEdit()">{{
+							translations[computedLanguageId].save
+						}}</el-button>
 					</div>
 				</div>
 			</el-dialog>
@@ -125,10 +131,13 @@ watchEffect(() => {
 		<Teleport to="body">
 			<el-dialog v-model="deletePopup" width="20%" style="border-radius: 5%; height: 20%">
 				<div class="delete">
-					{{translations[computedLanguageId].tableScript}}
+					{{ translations[computedLanguageId].tableScript }}
 					<div id="bottomButtons">
-						<el-button color="#ED5087" plain round @click="deletePopup = false">{{translations[computedLanguageId].no}}</el-button>
-						<el-button color="#ED5087" plain round @click="handleDelete()">{{translations[computedLanguageId].yes}}
+						<el-button color="#ED5087" plain round @click="deletePopup = false">{{
+							translations[computedLanguageId].no
+						}}</el-button>
+						<el-button color="#ED5087" plain round @click="handleDelete()"
+							>{{ translations[computedLanguageId].yes }}
 						</el-button>
 					</div>
 				</div>
@@ -143,17 +152,24 @@ watchEffect(() => {
 			<el-dialog v-model="addPopup" width="20%" style="border-radius: 5%; height: 22%">
 				<div id="addTablePopup" class="edit">
 					<div>
-						<span>{{translations[computedLanguageId].tableNumber}}</span
+						<span>{{ translations[computedLanguageId].tableNumber }}</span
 						><input id="addtableInput1" v-model="tableNumber" class="specialInput" />
 					</div>
 					<div style="padding-top: 2%">
-						<span>{{translations[computedLanguageId].tableCapacity}}</span
+						<span>{{ translations[computedLanguageId].tableCapacity }}</span
 						><input id="addtableInput2" v-model="tableCapacity" class="specialInput" />
 					</div>
 					<div style="padding-top: 5%">
-						<el-button color="#ED5087" plain round @click="addPopup = false">{{translations[computedLanguageId].cancel}}</el-button>
-						<el-button id="addTableButtonConfirm" color="#ED5087" plain round @click="handleAdd()"
-							>{{translations[computedLanguageId].add}}</el-button
+						<el-button color="#ED5087" plain round @click="addPopup = false">{{
+							translations[computedLanguageId].cancel
+						}}</el-button>
+						<el-button
+							id="addTableButtonConfirm"
+							color="#ED5087"
+							plain
+							round
+							@click="handleAdd()"
+							>{{ translations[computedLanguageId].add }}</el-button
 						>
 					</div>
 				</div>
@@ -161,12 +177,15 @@ watchEffect(() => {
 		</Teleport>
 	</ClientOnly>
 	<el-scrollbar>
-		<PageTitle :title=translations[computedLanguageId].tables></PageTitle>
+		<PageTitle :title="translations[computedLanguageId].tables"></PageTitle>
 		<main>
 			<div id="buttonContainer">
-				<SearchBar v-model="selectedTable" :options="tables.map((x) => (translations[computedLanguageId].table + ' ') + x.number)" />
+				<SearchBar
+					v-model="selectedTable"
+					:options="tables.map((x) => translations[computedLanguageId].table + ' ' + x.number)"
+				/>
 				<el-button id="addTableButton" color="#ED5087" plain round @click="addPopup = true">
-					+ {{translations[computedLanguageId].addTable}}</el-button
+					+ {{ translations[computedLanguageId].addTable }}</el-button
 				>
 			</div>
 			<div id="tableContainer">
